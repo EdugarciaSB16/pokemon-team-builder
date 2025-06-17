1a8f848a42502b5710f47f443d46d2db1aee4b77
Eduardo Garcia <edugarciasb16@gmail.com>
2025-06-17 03:43:12 +0200

    change folder structure

--- src/features/team/components/TeamSlotEmpty.jsx
+++
@@ -1,7 +0,0 @@
-export default function TeamSlotEmpty() {
-  return (
-    <div className="absolute inset-0 flex flex-col items-center justify-center opacity-20">
-      <div className="w-16 h-16 rounded-full border-4 border-dashed border-yellow-400 animate-pulse"></div>
-    </div>
-  );
-}
--- src/features/team/components/TeamSlotWrapper.jsx
+++
@@ -1,7 +0,0 @@
-export default function TeamSlotWrapper({ children }) {
-  return (
-    <div className="relative flex flex-col items-center justify-end w-32 h-32 bg-gradient-to-b from-gray-800 to-gray-900 rounded-lg shadow-lg border-2 border-yellow-400 transform transition-all hover:scale-105 hover:shadow-yellow-400/30 hover:z-10 overflow-hidden">
-      {children}
-    </div>
-  );
-}
