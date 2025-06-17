b8884dd7e341690aea8dc8ed1ccfc3074cef95c8
Eduardo Garcia <edugarciasb16@gmail.com>
2025-06-17 03:29:09 +0200

    chore(app): setup React Router and DnD context with HTML5 backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import Home from '@/app/Home';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <DndProvider backend={HTML5Backend}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </DndProvider>
   );
 }
