2d86271b4ea7d9b77691a56b372a9f4b76b0db2b
Eduardo Garcia <edugarciasb16@gmail.com>
2025-06-17 03:33:07 +0200

    refactor: grid layout in mobile responsive

--- src/features/pokemon/components/PokemonList.jsx
+++ src/features/pokemon/components/PokemonList.jsx
@@ -89,7 +89,7 @@ export default function PokemonList() {
 
   return (
     <div className="relative">
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 px-2">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 px-2">
         {pokemons.map((pokemon) =>
           pokemon && pokemon.id ? (
             <PokemonCard key={pokemon.id} pokemon={pokemon} />
