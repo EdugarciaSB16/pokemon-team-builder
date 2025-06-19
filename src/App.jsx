0518a82f0e7adc999c70dbcf801938549e3c7461
Eduardo Garcia <edugarciasb16@gmail.com>
2025-06-19 02:30:06 +0200

    feat(app): add battle route and wrap app with NotificationProvider

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import Home from '@/app/Home';
+import Battle from '@/app/Battle';
+import NotificationProvider from '@/components/NotificationProvider';
 
 export default function App() {
   return (
     <DndProvider backend={HTML5Backend}>
       <BrowserRouter>
+        <NotificationProvider />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/battle" element={<Battle />} />
         </Routes>
       </BrowserRouter>
     </DndProvider>
