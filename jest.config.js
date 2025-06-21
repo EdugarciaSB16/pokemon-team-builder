283123ba367848525db71180984cba01518455c3
Eduardo Garcia <edugarciasb16@gmail.com>
2025-06-21 12:30:10 +0200

    jest: fix route configuration

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,13 @@ export default {
     '^.+\\.[jt]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['js', 'jsx'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testEnvironmentOptions: {
+    customExportConditions: [''],
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(react-dnd|react-dnd-html5-backend|dnd-core|@react-dnd/invariant|@react-dnd/asap|@react-dnd/shallowequal)/)',
+  ],
 };
--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,24 @@
 import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder } from 'util';
+
+// Polyfills for Node.js environment
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+// Mock fetch for tests
+global.fetch = jest.fn();
+
+// Mock IntersectionObserver for tests
+global.IntersectionObserver = class {
+  constructor() {}
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+};
+
+// Mock crypto.randomUUID for tests
+Object.defineProperty(global, 'crypto', {
+  value: {
+    randomUUID: () => 'test-uuid-' + Math.random().toString(36).substr(2, 9),
+  },
+});
